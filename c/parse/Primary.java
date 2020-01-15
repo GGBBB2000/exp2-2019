28ee569da8621f7e88dea92c958094bc6328463e
GGBBB2000 <ggbbb@mineo.jp>
2020-01-16 00:24:31 +0900

    H4: 構文解析 構文解析用のクラスを追加

---
+++ c/parse/Primary.java
@@ -0,0 +1,22 @@
+package lang.c.parse;
+
+import lang.FatalErrorException;
+import lang.c.CParseContext;
+import lang.c.CParseRule;
+
+public class Primary extends CParseRule {
+    @Override
+    public void parse(CParseContext pcx) throws FatalErrorException {
+
+    }
+
+    @Override
+    public void semanticCheck(CParseContext pcx) throws FatalErrorException {
+
+    }
+
+    @Override
+    public void codeGen(CParseContext pcx) throws FatalErrorException {
+
+    }
+}
