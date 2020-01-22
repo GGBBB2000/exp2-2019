56a06473737db2df94b0deec7e294cd0dabab5ba
GGBBB2000 <ggbbb@mineo.jp>
2020-01-22 18:53:29 +0900

    H7: Condition系のファイルを追加

---
+++ c/parse/Condition.java
@@ -0,0 +1,124 @@
+package lang.c.parse;
+
+import lang.FatalErrorException;
+import lang.c.CParseContext;
+import lang.c.CParseRule;
+
+class Condition extends CParseRule {
+    @Override
+    public void parse(CParseContext pcx) throws FatalErrorException {
+
+    }
+
+    @Override
+    public void semanticCheck(CParseContext pcx) throws FatalErrorException {
+
+    }
+
+    @Override
+    public void codeGen(CParseContext pcx) throws FatalErrorException {
+
+    }
+}
+
+class ConditionLT extends CParseRule {
+    @Override
+    public void parse(CParseContext pcx) throws FatalErrorException {
+
+    }
+
+    @Override
+    public void semanticCheck(CParseContext pcx) throws FatalErrorException {
+
+    }
+
+    @Override
+    public void codeGen(CParseContext pcx) throws FatalErrorException {
+
+    }
+}
+
+class ConditionLE extends CParseRule {
+    @Override
+    public void parse(CParseContext pcx) throws FatalErrorException {
+
+    }
+
+    @Override
+    public void semanticCheck(CParseContext pcx) throws FatalErrorException {
+
+    }
+
+    @Override
+    public void codeGen(CParseContext pcx) throws FatalErrorException {
+
+    }
+}
+
+class ConditionGT extends CParseRule {
+    @Override
+    public void parse(CParseContext pcx) throws FatalErrorException {
+
+    }
+
+    @Override
+    public void semanticCheck(CParseContext pcx) throws FatalErrorException {
+
+    }
+
+    @Override
+    public void codeGen(CParseContext pcx) throws FatalErrorException {
+
+    }
+}
+
+class ConditionGE extends CParseRule {
+    @Override
+    public void parse(CParseContext pcx) throws FatalErrorException {
+
+    }
+
+    @Override
+    public void semanticCheck(CParseContext pcx) throws FatalErrorException {
+
+    }
+
+    @Override
+    public void codeGen(CParseContext pcx) throws FatalErrorException {
+
+    }
+}
+
+class ConditionEQ extends CParseRule {
+    @Override
+    public void parse(CParseContext pcx) throws FatalErrorException {
+
+    }
+
+    @Override
+    public void semanticCheck(CParseContext pcx) throws FatalErrorException {
+
+    }
+
+    @Override
+    public void codeGen(CParseContext pcx) throws FatalErrorException {
+
+    }
+}
+
+class ConditionNE extends CParseRule {
+    @Override
+    public void parse(CParseContext pcx) throws FatalErrorException {
+
+    }
+
+    @Override
+    public void semanticCheck(CParseContext pcx) throws FatalErrorException {
+
+    }
+
+    @Override
+    public void codeGen(CParseContext pcx) throws FatalErrorException {
+
+    }
+}
